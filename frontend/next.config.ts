ede5f58a22ce7883ebd473c34f2c888208c07332
Ammar Atiq <ammar.pixako1@gmail.com>
2025-10-31 12:23:51 +0500

    next-pwa works only with webpack not with webpack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,13 @@
 import type { NextConfig } from "next";
-
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  buildExcludes: [/app-build-manifest\.json$/],
 });
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {},
-  webpack: (config) => {
-    // ensure next-pwa works with webpack
-    return config;
-  },
 };
 
 export default withPWA(nextConfig);
