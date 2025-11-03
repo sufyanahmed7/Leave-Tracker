2a0e538626d6a3e7e7fbd0791059c1bd01d76340
Haris Raees <haris.pixako1@gmail.com>
2025-11-03 12:16:10 +0500

    cor change

--- backend/server.js
+++ backend/server.js
@@ -8,26 +8,7 @@ import leaveRoutes from "./src/routes/leaveRoutes.js";
 dotenv.config();
 const app = express();
 
- app.use((req, res, next) => {
-      const allowedOrigins = [
-        'https://leave-tracker-8j0ex8cyb-haris-raees-projects.vercel.app',
-        // Add any other specific origins that need access
-      ];
-      const origin = req.headers.origin;
-
-      if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-      
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add any custom headers your frontend sends
-      res.setHeader('Access-Control-Allow-Credentials', 'true'); // If you are sending cookies or authorization headers
-      if (req.method === 'OPTIONS') {
-        return res.sendStatus(200); // Handle preflight requests
-      }
-      next();
-    });
-
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 await connectDB(); // or call connectDB() depending on your implementation
